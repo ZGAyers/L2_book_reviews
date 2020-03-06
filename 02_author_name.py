6ac77882a3994807aab530a414dc81596df9d5ee
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-03-06 14:45:31 +1300

    End of day work - not fully working component 2

--- 02_author_name.py
+++ 02_author_name.py
@@ -1,12 +1,11 @@
 # Component 2 - Author Name
 
-author = []
+author = {}
 author_input = input("Author: ")
 
 if author_input == "":
     print("Since you left this field blank the author name will become 'Anonymous'.")
     author = "Anonymous"
-else:
-    author.append(author_input)
 
-print("The authors name is")
+
+print("The authors name is {}".format(author))
