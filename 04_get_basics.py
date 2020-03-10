e32048f5142f1fe0e9e1117ad2173d45aec8cd07
Zarah70879 <51766656+Zarah70879@users.noreply.github.com>
2020-03-11 10:46:32 +1300

    Working but unfinished component 04

--- 04_get_basics.py
+++ 04_get_basics.py
@@ -17,6 +17,29 @@ def text_helper(question, possible_answers, items_per_line, required=None):
 
         response = input("Choose: ")
 
+        # Checks that response has been given if required
+        if required == "yes" and response == "":
+            print(error)
+            continue
+
+        # check i fthe response is a number / choice, if it is, change
+        # it to the option in the list
+
+        try:
+            response = int(response) - 1
+            # Check that number is an option
+            if 0 <= int(response) < len(possible_answers):
+                # response is a choice in the list, return word
+                # that matches chosen number
+                response = possible_answers[response]
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            # User has typed in text, return it to the main routine
+            return response
+
 
 # --main routine--
 
@@ -25,9 +48,11 @@ genre_list = ["Action", "Adventure", "Detective", "Crime",
               "Fan-Fiction", "Anthology"]
 
 genre_list.sort()
-print(genre_list)
+
 
 # Makes everything in the list lowercase
 genre_list = [x.lower() for x in genre_list]
 
 genre = text_helper("Please choose a genre", genre_list, 4, "yes")
+
+print("Genre: {}".format(genre))
